6c210c5dc7a6b3e16e022aa804e9d4800c491771
semih1996 <semih.demirli@gatemarine.com>
2022-01-02 13:03:03 +0300

    Asp.net Web App Core  ve  API projleri oluşturuldu. Migration yapıldı.

---
+++ CovidChart/CovidChart.API/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidChart.API.Models
+{
+    public enum City
+    {
+        Istanbul = 1,
+        Ankara = 2,
+        Izmir = 3,
+        Trabzon = 4,
+        Urfa = 5
+    }
+}
